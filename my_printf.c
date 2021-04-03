42d15e637a47eba7643fca3cb01023612ed7cff2
Spencer Burke <info.spencer.burke@gmail.com>
2021-04-02 22:39:43 -0700

    Pushing small changes and trying to test each function while I refamiliarize myself with the codebase.

--- my_printf.c
+++ my_printf.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int
-my_printf(const char *fmt, ...)
+int my_printf(const char *fmt, ...);
+int str_len(const char *str);
+int m_print(const char *str, int f_descriptor);
+
+int m_printf(const char *fmt, ...)
+//the printf function
 {
     va_list arg_list; 
     int return_code = 0;
@@ -12,14 +16,48 @@ my_printf(const char *fmt, ...)
     va_start(arg_list, fmt);
     while ( *fmt)
     {
-        puts(fmt);
+       //pass 
     }
     va_end(arg_list);
 
     return return_code; 
 }
 
+int str_len(const char *str)
+//return the length of the string
+{
+    int len = 0;
+    int i;
+
+    for(int i = 0; i != '\0'; i++)
+        len++;
+
+    return len;
+}
+
+int m_print(const char *str, int f_descriptor)
+//writes a string to stdout or other nonzero file descriptor
+{
+    int r_code = 0;
+    int len = str_len(str); 
+
+    if(f_descriptor > 1)
+       f_descriptor = 1;
+    else
+        f_descriptor = f_descriptor;
+	
+	if((fwrite(f_descriptor, str, len)) != len)
+	{
+		fwrite(2, "Error occurred", 16);
+        r_code = 84;
+	}
+    
+    return r_code;
+}
+
 int main()
 {
-    my_printf("Hello world");
+    //my_printf("Hello world");
+    m_print("Hello world", 1);
 }
+
