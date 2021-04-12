71a88f9c765f549f9f209256e3af88f1759d7827
Spencer Burke <info.spencer.burke@gmail.com>
2021-04-12 13:53:10 -0700

    Added some code to m_print, and got a basic version working without any error handling.

--- my_printf.c
+++ my_printf.c
@@ -1,10 +1,9 @@
 #include <stdarg.h>
 #include <unistd.h>
-#include <stdio.h>
 
 int my_printf(const char *fmt, ...);
 int str_len(const char *str);
-int m_print(const char *str, int f_descriptor);
+int m_print(const char *str, int f_descriptor, int size);
 char* convert(unsigned int, int);
 
 int m_printf(const char *fmt, ...)
@@ -37,37 +36,46 @@ int str_len(const char *str)
     return len;
 }
 
-int m_print(const char *str, int f_descriptor)
+/// int m_print(const char *str, int f_descriptor)
+/// //writes a string to stdout or other nonzero file descriptor
+/// {
+///     int r_code = 0;
+///     int len = str_len(str); 
+/// 
+///     if(f_descriptor > 1)
+///        f_descriptor = 1;
+///     else
+///         f_descriptor = f_descriptor;
+/// 	
+/// 	if((write(f_descriptor, str, len)) != len)
+/// 	{
+/// 		write(2, "Error occurred", 16);
+///         r_code = 84;
+/// 	}
+///     
+///     return r_code;
+/// }
+
+int m_print(const char *str, int f_descriptor, int size) 
 //writes a string to stdout or other nonzero file descriptor
 {
     int r_code = 0;
-    int len = str_len(str); 
-
-    if(f_descriptor > 1)
-       f_descriptor = 1;
-    else
-        f_descriptor = f_descriptor;
-	
-	if((write(f_descriptor, str, len)) != len)
-	{
-		write(2, "Error occurred", 16);
-        r_code = 84;
-	}
-    
+    int bytes_written = write(f_descriptor, str, size);
     return r_code;
 }
 
-void basic_print(const char *str, int f_descriptor)
+void basic_print(const char *str, int f_descriptor, int size)
 // just a small method for refining and working on the write syscall **WILL DELETE LATER**
 // supplying a static 11 because it is only supposed to print the "hello world" string
 {
-    write(f_descriptor, str, 11);
+    write(f_descriptor, str, size);
 }
 
 int main()
 {
-    const char *str = "Hello World\n\0";
+    const char *str = "Hello World\n";
     char string[13] = "Hello World\n";
-    printf("%d\n", str_len(str));
+    //basic_print(string, 1, sizeof(string));
+    m_print(str, 1, str_len(str));
 }
 
