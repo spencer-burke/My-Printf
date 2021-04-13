e4ac8efbb7124dbc266decde9ad7cffdd0bfb944
Spencer Burke <info.spencer.burke@gmail.com>
2021-04-13 14:06:02 -0700

    Implemented a less basic version of m_print with some error handling, and started working on the final m_printf function.

--- my_printf.c
+++ my_printf.c
@@ -1,21 +1,21 @@
 #include <stdarg.h>
 #include <unistd.h>
+#include <errno.h>
 
 int my_printf(const char *fmt, ...);
 int str_len(const char *str);
 int m_print(const char *str, int f_descriptor, int size);
 char* convert(unsigned int, int);
 
-int m_printf(const char *fmt, ...)
+int m_printf(char *fmt, ...)
 //the printf function
 {
     va_list arg_list; 
     int return_code = 0;
-    char result[sizeof(fmt)]; 
     int strlen = str_len(fmt);
 
     va_start(arg_list, fmt);
-    while ( *fmt)
+    while (*fmt)
     {
        //pass 
     }
@@ -36,39 +36,18 @@ int str_len(const char *str)
     return len;
 }
 
-/// int m_print(const char *str, int f_descriptor)
-/// //writes a string to stdout or other nonzero file descriptor
-/// {
-///     int r_code = 0;
-///     int len = str_len(str); 
-/// 
-///     if(f_descriptor > 1)
-///        f_descriptor = 1;
-///     else
-///         f_descriptor = f_descriptor;
-/// 	
-/// 	if((write(f_descriptor, str, len)) != len)
-/// 	{
-/// 		write(2, "Error occurred", 16);
-///         r_code = 84;
-/// 	}
-///     
-///     return r_code;
-/// }
-
 int m_print(const char *str, int f_descriptor, int size) 
 //writes a string to stdout or other nonzero file descriptor
 {
     int r_code = 0;
     int bytes_written = write(f_descriptor, str, size);
+    char error_msg[8] = "[Error]\n";
+    if(bytes_written != size)
+    {
+        r_code = 84;
+        write(2, error_msg, 8);
+    } 
     return r_code;
-}
-
-void basic_print(const char *str, int f_descriptor, int size)
-// just a small method for refining and working on the write syscall **WILL DELETE LATER**
-// supplying a static 11 because it is only supposed to print the "hello world" string
-{
-    write(f_descriptor, str, size);
 }
 
 int main()
