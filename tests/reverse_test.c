e7ae817eca04c30c24ae6975a69d4fbd11bed245
Spencer Burke <info.spencer.burke@gmail.com>
2021-08-15 23:46:41 -0700

    Add the basic code the "reverse" function unit test

--- tests/reverse_test.c
+++ tests/reverse_test.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include "minunit.h"
-
+#include "../src/my_printf.h"
 int tests_run = 0;
 char correct_string[5] = "olleh";
 char input_string[5] = "hello";
 
 static char* test_reverse()
 {
-    
+    reverse(input_string, 5);
+    int correct = strcmp(correct_string, input_string);
+    mu_assert("[ERROR]: input_string != correct_string", correct == 0);
+
+    return 0;
 }
